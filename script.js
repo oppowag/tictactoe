fe2fee2f62f335882c818caf834b1dd7d8ddc14e
oppowag <65634715+oppowag@users.noreply.github.com>
2020-06-04 20:37:10 -0700

    Added logic to click, check eligibility, and add item to grid

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
 const gameBoard = (() => {
-    board = [];
+    var board = ["", "", "", "", "", "", "", "", ""];
+
+    // Cache DOM
+    var grid = document.getElementsByClassName("grid-container")[0];
+    var gridItems = [];
+    for(i = 0; i < grid.getElementsByTagName("div").length; i++){
+        gridItems.push(grid.getElementsByTagName("div")[i]);
+    }
+
+    function _render(index){
+        gridItems[index].innerHTML = board[index];
+    }
+
+    function addMove(index, value){
+        board.splice(index, 1, value);
+        _render(index);
+    }
+
+    return{
+        gridItems,
+        addMove,
+    };
 })();
 
 const displayController = (() => {
+    // Attach EventListeners to each grid item
+    gameBoard.gridItems.forEach((item, index) => {
+        item.addEventListener("click", () => {
+            _checkEligibility(item, index);
+        });
+    });
+
+    function _checkEligibility(item, index){
+        if(item.innerHTML === ""){
+            gameBoard.addMove(index, "X");
+        }
+    }
 
 })();
 
