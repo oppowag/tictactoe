9b597d73941a0d6127d7210c5ee11276b5ce68aa
oppowag <65634715+oppowag@users.noreply.github.com>
2020-06-05 19:59:27 -0700

    Finished everything except asking for name and displaying winning person

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const gameBoard = (() => {
     var board = ["", "", "", "", "", "", "", "", ""];
+    var gameOver = false;
 
     // Cache DOM
     var grid = document.getElementsByClassName("grid-container")[0];
@@ -7,23 +8,16 @@ const gameBoard = (() => {
     for(i = 0; i < grid.getElementsByTagName("div").length; i++){
         gridItems.push(grid.getElementsByTagName("div")[i]);
     }
-    var startButton = document.getElementById("startButton");
+
+    // Attach EventListeners to each grid item
+    gridItems.forEach((item, index) => {
+        item.addEventListener("click", () => {
+            displayController.checkEligibility(item, index);
+        });
+    });
 
     function _render(index){
         gridItems[index].innerHTML = board[index];
-    }
-
-    function reset(){
-        board = ["", "", "", "", "", "", "", "", ""];
-        for(i = 0; i < gridItems.length; i++){
-            _render(i);
-        }
-    }
-
-    function addMove(index, value){
-        board.splice(index, 1, value);
-        _render(index);
-        _checkGame(index);
     }
 
     function _checkGame(index){       
@@ -32,28 +26,45 @@ const gameBoard = (() => {
             // Check columns
             for(i = 0; i < 3; i++){
                 if(board[i] != "" && board[i] == board[i+3] && board[i] == board[i+6]){
-                    displayController.updateDisplay(board[i]);
+                    displayController.declareWinner(board[i]);
                     return;
                 }
             }
             // Check rows
             for(i = 0; i <= 6; i += 3){
                 if(board[i] != "" && board[i] == board[i+1] && board[i] == board[i+2]){
-                    displayController.updateDisplay(board[i]);
+                    displayController.declareWinner(board[i]);
                     return;
                 }
             }
             // Check diagonals
             if(board[0] != "" && board[0] == board[4] && board[0] == board[8]){
-                displayController.updateDisplay(board[0]);
+                displayController.declareWinner(board[0]);
+                return;
             }
-            else if(board[2] != "" && board[0] == board[4] && board[0] == board[6]){
-                displayController.updateDisplay(board[2]);
+            else if(board[2] != "" && board[2] == board[4] && board[2] == board[6]){
+                displayController.declareWinner(board[2]);
+                return;
             }
         }
-        else if(countTurns() == 9){
-            displayController.updateDisplay("tie");
+        if(countTurns() == 9){
+            displayController.declareWinner("tie");
         }
+    }
+
+    function reset(){
+        board = ["", "", "", "", "", "", "", "", ""];
+        for(i = 0; i < gridItems.length; i++){
+            _render(i);
+        }
+        gameBoard.gameOver = false;
+        gameTracker.wipeAway();
+    }
+
+    function addMove(index, value){
+        board.splice(index, 1, value);
+        _render(index);
+        _checkGame(index);
     }
 
     function countTurns(){
@@ -61,26 +72,34 @@ const gameBoard = (() => {
     }
 
     return{
-        gridItems,
         addMove,
         countTurns,
-        startButton,
         reset,
+        gameOver,
     };
 })();
 
-const displayController = (() => {
-    // Attach EventListeners to each grid item
-    gameBoard.gridItems.forEach((item, index) => {
-        item.addEventListener("click", () => {
-            _checkEligibility(item, index);
-        });
-    });
-    // Attach EventListener to button
-    gameBoard.startButton.addEventListener("click", gameBoard.reset);
+const gameTracker = (() => {
+    // Cache DOM
+    var startButton = document.getElementById("startButton");
+    var resultSection = document.getElementsByClassName("belowGrid")[0];
 
-    function _checkEligibility(item, index){
-        if(item.innerHTML === ""){
+    // Attach EventListener to button
+    startButton.addEventListener("click", gameBoard.reset);
+
+    function wipeAway(){
+        resultSection.innerHTML = "";
+    }
+
+    return{
+        resultSection,
+        wipeAway,
+    }
+})();
+
+const displayController = (() => {
+    function checkEligibility(item, index){
+        if (!gameBoard.gameOver && item.innerHTML === ""){
             if(gameBoard.countTurns() % 2 == 0){
                 gameBoard.addMove(index, "X");
             }
@@ -90,16 +109,28 @@ const displayController = (() => {
         }
     }
 
-    function updateDisplay(result){
-        console.log(result);
+    function declareWinner(result){
+        var resultH2 = document.createElement("h2");
+        resultH2.innerHTML = result;
+        gameTracker.resultSection.appendChild(resultH2);
+
+        gameBoard.gameOver = true;
     }
 
     return{
-        updateDisplay,
+        checkEligibility,
+        declareWinner,
     }
 
 })();
 
-const Player = () => {
+const Player = (name, symbol) => {
+    const getName = () => name;
+    const getSymbol = () => symbol;
+
+    return{
+        getName,
+        getSymbol,
+    }
 
 };
